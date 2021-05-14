268b3d86a89add7d42197d4133f7fa4e6e969021
Fabko123 <71120191+Fabko123@users.noreply.github.com>
2021-05-14 15:41:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,121 @@
+const container = document.createElement('div')
+container.setAttribute('class','grid')
+bleble.appendChild(container)
+
+const nazwa = document.createElement('div')
+const nazwa3 = document.createElement('h2')
+const nazwa2 = document.createElement('p')
+const btn = document.createElement('button')
+const btn1 = document.createElement('button1')
+nazwa3.innerText="ksiazka1"
+nazwa2.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+btn.innertText="Usuń"
+btn1.innertText="Edytuj"
+nazwa.setAttribute('class','grid2')
+btn.setAttribute('class','button')
+btn.setAttribute('onclick','remove')
+btn1.setAttribute('class','button')
+nazwa.appendChild(nazwa3)
+nazwa.appendChild(nazwa2)
+nazwa.appendChild(btn)
+nazwa.appendChild(btn1)
+container.appendChild(nazwa)
+
+
+const nazwa5 = document.createElement('div')
+const nazwa6 = document.createElement('h2')
+const nazwa7 = document.createElement('p')
+const btn2 = document.createElement('button')
+const btn3 = document.createElement('button1')
+nazwa6.innerText="ksiazka1"
+nazwa7.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+btn.innertText="Usuń"
+btn1.innertText="Edytuj"
+nazwa5.setAttribute('class','grid2')
+btn2.setAttribute('class','button')
+btn2.setAttribute('onclick','remove')
+btn3.setAttribute('class','button')
+nazwa5.appendChild(nazwa6)
+nazwa5.appendChild(nazwa7)
+nazwa5.appendChild(btn2)
+nazwa5.appendChild(btn3)
+container.appendChild(nazwa5)
+
+const nazwa8 = document.createElement('div')
+const nazwa9 = document.createElement('h2')
+const nazwa10 = document.createElement('p')
+const btn4 = document.createElement('button')
+const btn5 = document.createElement('button1')
+nazwa9.innerText="ksiazka1"
+nazwa10.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+btn4.innertText="Usuń"
+btn5.innertText="Edytuj"
+nazwa8.setAttribute('class','grid2')
+btn4.setAttribute('class','button')
+btn4.setAttribute('onclick','remove')
+btn5.setAttribute('class','button')
+nazwa8.appendChild(nazwa9)
+nazwa8.appendChild(nazwa10)      
+nazwa8.appendChild(btn4)
+nazwa8.appendChild(btn5)
+container.appendChild(nazwa8)
+
+const nazwa11 = document.createElement('div')
+const nazwa12 = document.createElement('h2')
+const nazwa13 = document.createElement('p')
+const btn6 = document.createElement('button')
+const btn7 = document.createElement('button1')
+nazwa12.innerText="ksiazka1"
+nazwa13.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+btn6.innertText="Usuń"
+btn7.innertText="Edytuj"
+nazwa11.setAttribute('class','grid2')
+btn6.setAttribute('class','button')
+btn6.setAttribute('onclick','remove')
+btn7.setAttribute('class','button')
+nazwa11.appendChild(nazwa12)
+nazwa11.appendChild(nazwa13)      
+nazwa11.appendChild(btn6)
+nazwa11.appendChild(btn7)
+container.appendChild(nazwa11)
+
+const nazwa14 = document.createElement('div')
+const nazwa15 = document.createElement('h2')
+const nazwa16 = document.createElement('p')
+const btn8 = document.createElement('button')
+const btn9 = document.createElement('button1')
+nazwa15.innerText="ksiazka1"
+nazwa16.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+btn8.innertText="Usuń"
+btn9.innertText="Edytuj"
+nazwa14.setAttribute('class','grid2')
+btn8.setAttribute('class','button')
+btn8.setAttribute('onclick','remove')
+btn9.setAttribute('class','button')
+nazwa14.appendChild(nazwa15)
+nazwa14.appendChild(nazwa16)      
+nazwa14.appendChild(btn8)
+nazwa14.appendChild(btn9)
+container.appendChild(nazwa14)
+
+const nazwa17 = document.createElement('div')
+const nazwa18 = document.createElement('h2')
+const nazwa19 = document.createElement('p')
+const btn10 = document.createElement('button')
+const btn11 = document.createElement('button1')
+nazwa18.innerText="ksiazka1"
+nazwa19.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+btn10.innertText="Usuń"
+btn11.innertText="Edytuj"
+nazwa17.setAttribute('class','grid2')
+btn10.setAttribute('class','button')
+btn10.setAttribute('onclick','remove')
+btn11.setAttribute('class','button')
+nazwa17.appendChild(nazwa18)
+nazwa17.appendChild(nazwa19)      
+nazwa17.appendChild(btn10)
+nazwa17.appendChild(btn11)
+container.appendChild(nazwa17)
+
+
+
